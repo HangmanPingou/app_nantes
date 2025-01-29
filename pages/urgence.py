dc130c958b119edcaaec73678b499e3b64ad3701
hangmanpingou <xxhangmanxx@free.fr>
2025-01-29 14:23:17 +0100

    Ajout de la location sur la carte urgence et des 5 plus proches défibrilateur. MAJ du requierements.txt

--- pages/urgence.py
+++ pages/urgence.py
@@ -2,7 +2,8 @@ import streamlit as st
 import folium
 import pandas as pd
 import requests
-from streamlit_folium import st_folium
+import geopy.distance
+from streamlit_folium import st_folium, folium_static
 from pathlib import Path
 
 headers = {   # Pour se connecter à l'API openstreetmap.
@@ -17,6 +18,13 @@ def API_address(postal_address):   # Fonction qui récupère les coordonnées GP
     r = requests.get(link, headers=headers).json()
     return float(r[0]["lat"]), float(r[0]["lon"])
 
+def tri_liste(liste, indice, valeur): # Fonction de trie pour intégrer une valeur dans une liste pour rester en ordre croissant.
+    longueur = len(liste)
+    for _ in range(longueur, indice +1, -1):
+        liste[_ - 1] = liste[_ - 2]
+    liste[indice] = valeur
+    return liste
+
 st.title("Carte des défibrilateurs.")
 
 add = Path.cwd()/"data"
@@ -24,27 +32,42 @@ add = Path.cwd()/"data"
 df = pd.read_csv(add/"defibrilateur.csv")
 
 liste_coor = [dict(eval(coor)).get("geometry").get("coordinates")[::-1] for coor in df.geo_shape]
-
-liste_lieux = [lieux for lieux in df.designation]
-
+liste_lieux = [lieux for lieux in df.designation]           # Création des différentes listes par rapport au fichier.
 liste_addresses = [adresse for adresse in df.adresse]
 
-m = folium.Map((47.219940, -1.573184), zoom_start = 12)
+m = folium.Map((47.219940, -1.573184), zoom_start = 12)  # Initialisation de la carte.
 
-for coord, lieux, adresse in zip(liste_coor, liste_lieux, liste_addresses):   # Ajout des marqueurs pour chaque défibrilateurs.
+for coord, lieux, adresse in zip(liste_coor, liste_lieux, liste_addresses):   # Ajout des marqueurs pour chaque défibrilateur.
     marker = folium.Marker(coord, popup= f"{lieux} / {adresse}", icon=folium.Icon(color='green'))
     marker.add_to(m)
 
-if "marker2" in st.session_state:
+if "markers" not in st.session_state:  # Liste des marqueurs à rajouter, la liste étant créée dans la session.
+    st.session_state.markers = []
+
+if "marker2" in st.session_state:  # Permet de gérer le marqueur "VOUS ETES ICI" dans la session.
     marker2 = st.session_state.marker2
     marker2.add_to(m)
+
+for indice in st.session_state.markers:  # Ajouts des marqueurs présent dans la liste de la session.
+    folium.Marker(liste_coor[indice], popup= f"{liste_lieux[indice]} / {liste_addresses[indice]}").add_to(m)
 
 here = st.text_input("Indiquez l'adresse où vous êtes.")
 
 if st.button("Se localiser."):
-    coor_here = API_address(here)
+    coor_here = API_address(here)  # Utilise la fonction pour récupérer les coordonnées GPS à partir de l'adresse entrée dans le champ.
     marker2 = folium.Marker(coor_here, popup= "VOUS ÊTES ICI", icon= folium.Icon(color='red'))
     marker2.add_to(m)
-    st.session_state["marker2"] = marker2
+    st.session_state["marker2"] = marker2 # Enregistre ce marqueur dans la session.
+    nb_fois = 5  # Variable qui donne le nombre de marqueurs plus proches à afficher.
+    liste_proche = [[50000, 0]] * nb_fois
+    for coor in liste_coor:  # Calcule la distance de chaque point avec celui où on est et ressort une liste avec la distance et l'indice des 5 plus proche.
+        for indice in range(len(liste_proche)):
+            if round(geopy.distance.geodesic(coor_here, coor).m) < liste_proche[indice][0]:
+                valeur = [round(geopy.distance.geodesic(coor_here, coor).m), liste_coor.index(coor)]
+                liste_proche = tri_liste(liste_proche, indice, valeur)
+                break
+    liste_indice = [liste_proche[indice][1] for indice in range(len(liste_proche))]
+    for indice in liste_indice:  # Ajoute à la liste de la session les indices des 5 plus proches.
+        st.session_state.markers.append(indice)
 
 st_folium(m, width=725)
